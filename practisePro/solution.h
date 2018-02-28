011be8ae3d9ce5dfa24b84b6566b691389ee93bf
alex-dell <2235317212@qq.com>
2018-02-28 19:44:01 +0800

    fix

--- practisePro/solution.h
+++ practisePro/solution.h
@@ -62,14 +62,32 @@ public:
 
     BinaryTreeNode *rebuiltTree(int *, int *, int);  //No.6 focus!!! ***
 
+    //No.7 思路有点错误 notice
+    template<typename T> class CQueue{
+    public:
+        CQueue ();
+        ~CQueue ();
+
+        void appendTail(const T&);
+        T deleteHead();
+
+    private:
+        stack<T> s1;
+        stack<T> s2;
+    };
+
+    //quickSort
+    void quickSort(int[], int, int, int);
+    void quickSort(int[], int, int);
+    void quickSort2(int[], int, int);
+
+    int minNum(int*, int);//No.8
     void printNDigits(int);  //No.12
     void deleteNode(ListNode *, ListNode *);  //No.13
     void reorderList(int *, int n);  //No.14
-
     ListNode *theLastKthNode(ListNode *, int);  //No.15
     ListNode *reverseList(ListNode *);  //No.16
     ListNode *mergeList(ListNode *, ListNode *);  //No.17
-
     bool isSubTree(BinaryTreeNode *, BinaryTreeNode *);  //No.18
     void printMatrix(int **, int, int);  //N0.20
 
@@ -89,35 +107,16 @@ public:
 
     bool isValidOrder(int *, int *, int);  //No.22
     void printBTreeFromTopToBottom(BinaryTreeNode *); //No.23, notice
+
     bool isPostOrder(int *, int); //No.24
-
-    //No.7 思路有点错误
-    template<typename T> class CQueue{
-    public:
-        CQueue ();
-        ~CQueue ();
-
-        void appendTail(const T&);
-        T deleteHead();
-
-    private:
-        stack<T> s1;
-        stack<T> s2;
     void findSumPath(BinaryTreeNode *, int);  //No.25
     void findSumPath(BinaryTreeNode *, int, vector<BinaryTreeNode *>);  //No.25
 
-    };
-
-    //quickSort
-    void quickSort(int[], int, int, int);
-    void quickSort(int[], int, int);
-    void quickSort2(int[], int, int);
-
-    int minNum(int*, int);//No.8
-
     ComplexListNode *clone(ComplexListNode *);  //No.26 focus!!!
+
     BinaryTreeNode *treeToList(BinaryTreeNode *);  //No.27 notice book's function
     void permutation(char *);  //No.28
+
     void combination();  //focus!!!
 
     int moreThanHalfNum(int *, int);  //No.29
@@ -126,23 +125,23 @@ public:
     int numberOf1Between1AndN(int); //No.32 notice
 
     void printMinNumber(int *, int);  //No.33 注意qsort的使用
-
     int getUglyNumber(int);  //No.34 notice
 
     char firstNotRepeatChar(char *); //No.35
+
     int inversePairs(int *, int); //No.36 focus,归并排序的变种。。。
 
     ListNode *findFirstInCommon(ListNode *, ListNode *); //No.37
     int getNumberOfK(int *, int, int); //No.38 notice
 
     int deepOfTree(BinaryTreeNode *); //No.39
+
     void findNumAppendOnce(int *, int, int *, int *); //No.40 focus
 
     bool findNumbersWithSum(int *, int, int *, int *, int);  //No.41
     char *reverseSentence(char *);  //No.42
-
     void printProbability(int); //No.43 notice
 };
 
-#endif // SOLUTION
+#endif //SOLUTION
 
